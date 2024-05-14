79bbb4080124e99d8be1bccb6a47b6ae3b4b7618
Alexander Toscano Ricardo <atoscano@correo.unicordoba.edu.co>
2024-05-14 17:40:37 -0500

    Refactorize elements

--- components/Elements/ET/InputTextField.vue
+++ components/Elements/ET/InputTextField.vue
@@ -18,16 +18,20 @@
 </template>
 
 <script setup>
+// se importan las referencia reactivas y computadas
 import { ref, computed } from "vue";
+// son los datos que se recolectan de un formualrio
 const data = ref("");
+// existe una sola props que es el elemento que se va a renderizar y esta contiene las demas propiedades
 const props = defineProps({
   element: {
     type: Object,
     required: true,
   },
 });
-
+// en class se recibe un array se transforma a string
 const combinedClasses = computed(() => {
+  // computed se utiliza para transformar los datos que se reciben
   const classes = Array.isArray(props.element.properties.class)
     ? props.element.properties.class
     : [];
